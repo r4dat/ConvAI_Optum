20a545839470e61ec6e8a1897f16f61d74190d06
Rob <r4dat@users.noreply.github.com>
2022-08-23 11:15:17 -0400

    Update DecisionTreeTask.py
    Add base confusion matrix as well as visual with labels from matplotlib.

--- DecisionTreeTask.py
+++ DecisionTreeTask.py
@@ -2,7 +2,9 @@ from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt  
 
 data = load_iris()
 
@@ -23,4 +25,7 @@ print(predictions)
 # Accuracy 
 score = accuracy_score(test_y,predictions)  
 print(f'Accuracy score is {score:3.3f}')
+print(confusion_matrix(test_y,predictions))
+plot_confusion_matrix(model,test_x,test_y)
+plt.show
 
