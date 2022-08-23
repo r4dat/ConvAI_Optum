b2cd31935f2508897c25115751d6727564cbaea8
Rob <r4dat@users.noreply.github.com>
2022-08-23 12:05:31 -0400

    Add label names for reference.
    Add label names, remove some matplotlib calls.

--- DecisionTreeTask.py
+++ DecisionTreeTask.py
@@ -12,6 +12,8 @@ data = load_iris()
 
 features = data["data"]
 labels = data["target"]
+names = data["target_names"]
+print(data["target_names"])
 
 # Split 80/20 
 train_x, test_x, train_y, test_y = train_test_split(features, labels, test_size=0.2, random_state=7)
@@ -27,8 +29,6 @@ print(predictions)
 score = accuracy_score(test_y,predictions)  
 print(f'Accuracy score is {score:3.3f}')
 print(confusion_matrix(test_y,predictions))
-#plot_confusion_matrix(model,test_x,test_y)
-#plt.show
     
 # Test depth options
 tree_depths = [1,5,10]
@@ -40,4 +40,7 @@ print (cv_model.best_score_, cv_model.best_params_)
 final_predict = final_model.predict(test_x)
 print(confusion_matrix(test_y,final_predict))
 ConfusionMatrixDisplay.from_predictions(test_y,final_predict)
-plt.show()
+#plt.show()
+
+## Label 1 (Versicolor) has the most false-positives. 10 TP, 2 FP on the hold-out set. 
+## I didn't expect the same confusion matrix after CV but we've made it all deterministic so it is what it is.
