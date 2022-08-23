a2e5f220fdbc80872f252ddc40805b184da1fc08
Rob <r4dat@users.noreply.github.com>
2022-08-23 10:54:04 -0400

    Update DecisionTreeTask.py
    Split dataset, print accuracy score.

--- DecisionTreeTask.py
+++ DecisionTreeTask.py
@@ -1,15 +1,26 @@
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 data = load_iris()
+
 
 features = data["data"]
 labels = data["target"]
 
+# Split 80/20 
+train_x, test_x, train_y, test_y = train_test_split(features, labels, test_size=0.2, random_state=7)
+
 model = DecisionTreeClassifier()
 
-model.fit(features, labels)
+model.fit(train_x, train_y)
 
-predictions = model.predict(features)
+predictions = model.predict(test_x)
 
 print(predictions)
+# Accuracy 
+score = accuracy_score(test_y,predictions)  
+print(f'Accuracy score is {score:3.3f}')
+
